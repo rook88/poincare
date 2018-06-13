7ca5ffb01a0e7780aeb814ca5c94c7c08eca02c8
RookDell <Rook@d.ell>
2018-06-13 23:10:13 +0200

    Zoom

--- walkthroughLabels.py
+++ walkthroughLabels.py
@@ -20,9 +20,9 @@ class gonClass():
                 gonIter = gonClass(p, q, r, path[1:])
                 self.gon = gonIter.gon.getMirror(int(path[0]))
         self.color = colorFromZ(self.gon.center.z, self.path)
-#        self.color = colorFromPath(self.path)
+        self.direction = np.imag(np.log(self.gon.center.z ** p))
     def __str__(self):
-        ret = "Gon, label = {}, polygon = {}".format(self.label, self.gon)
+        ret = "Gon, label = {}, polygon = {}, direction = {}".format(self.label, self.gon, self.direction)
         return ret
 
 def getGon(p, q, r, label):
@@ -33,6 +33,7 @@ def getGon(p, q, r, label):
         return iter.getMirror(int(label[0]))
 
 theta = 1.5 - np.sqrt(5) * 0.5 
+
 
 def colorFromLabel(label):
     hue = int(len(label) * theta * 180) % 180
@@ -47,14 +48,14 @@ def colorFromPath2(path):
     hue = int(hue) % 180
     return (hue, 255, 255)
 
-def colorFromPath3(z, path, weight):
+def colorFromPath3(z, path, weightRaw):
+    weight = np.sqrt(weightRaw)
     (hue1, saturation1, value1) = colorFromZ(z, path)
-    value2 = 1 + ((len(path) + 2) % 3) * 127
+    value2 = 1 + ((len(path) + 1) % 2) * 254
     saturation = int(weight * saturation1)
     value = int(weight * value1 + (1 - weight) * value2)
     hue = hue1
     return (hue, saturation, value)
-
 
 def colorFromPath(path):
     hue = 0.0
@@ -73,9 +74,10 @@ def colorFromPath(path):
     
 def colorFromZ(z, path):
     hue = int(np.imag(np.log(z)) / np.pi * 90) % 180
-    print z, hue
+#    print z, hue
     if path:
-        value = int(255 * (3.0 / (2.0 + len(path))))
+#        value = int(255 * (3.0 / (2.0 + len(path))))
+        value = 255
         saturation = 255
     else:
         value = 255
@@ -94,28 +96,8 @@ def isNewGon(gNew):
                 return False
     return True
 
-def iterLabel(label):
-    temp = gonClass(p, q, gonRadius, label)
-    gons[str(label)] = temp
-    gonTemp = temp.gon
-    gonColor = colorFromLabel(label)
-    if isNewGon(temp):
-        print("New: {}".format(temp))
-        pImg.drawPolygon(gonTemp, color = gonColor)
-        for zerolabel in zerolabels:
-            if label:
-                nextLabelItem = (zerolabel + label[-1] - 1) % p + 1
-            else:
-                nextLabelItem = zerolabel
-            newLabel = label + [nextLabelItem]
-            iterLabel(newLabel)
-    else:
-        print("Old: {}".format(temp))
-
-
 def iterLabels(n):
     generation = [gons[l] for l in gons if len(gons[l].path) == n]
-    generation = [g for g in gonsTable if len(g.path) == n]
     for gOld in generation:
         for zerolabel in zerolabels:
             if gOld.path:
@@ -125,24 +107,17 @@ def iterLabels(n):
                 nextLabelItem = zerolabel
             newPath = gOld.path + [nextLabelItem]
             temp = gonClass(p, q, gonRadius, newPath)
-#            gonColor = colorFromPath(temp.path)
-#            gonColor = colorFromZ(temp.gon.center.z, temp.path)
             if isNewGon(temp):
                 gons[temp.label] = temp
-                gonsTable.append(temp)
-#                print("New: {}".format(temp))
-#                pImg.drawPolygon(temp.gon, color = gonColor)
-            else:
-                pass
-#                print("Old: {}".format(temp))
+
  
-def showImg(pImg):
-    img = getImg(pImg)
+def showImg(pImg, f = 1.0):
+    img = getImg(pImg, f = f)
     cv2.imshow('myImg', img)
     cv2.imwrite("lastPolygon.jpg", img)
     cv2.waitKey(0)
 
-def getImg(pImg, color = "rgb"):
+def getImg(pImg, color = "rgb", f = 1.0):
     multiplier = pImg.multiplier
     img = pImg.getImg(multiplier = multiplier)
     if color == "rgb":
@@ -154,19 +129,21 @@ def getImg(pImg, color = "rgb"):
     img = cv2.resize(img, dsize = (800, 800))
     return img
 
-def iterImg(multiplier = 1.0):
-    pImg = poincare.poincareImg(radius = 900, pointRadius = 5, size = [2000, 2000], backgroundIsWhite = True)
+def iterImg(multiplier = 1.0, reverse = False, radiusFactor = 1.0):
+    radius = int(900 * radiusFactor)
+    pImg = poincare.poincareImg(radius = radius, pointRadius = 5, size = [2000, 2000], backgroundIsWhite = True)
     pImg.multiplier = multiplier
-#    if gon.label in ["", "1", "1-2", "1-2-3", "1-2-3-1", "1-2-3-1-2"] or 1 == 0:
-    for gon in gonsTable:
-#    gon = gons[label]
+    if reverse:
+        tempTable = reversed(gonsTable)
+    else:
+        tempTable = gonsTable
+    for gon in tempTable:
         gonNew = getGon(p, q, gonRadius / multiplier, gon.path)
-        if gon.label == "1-2-3-1" or 1 == 1:
-            color = gon.color
-            color = colorFromPath3(gon.gon.center.z, gon.path, 1.0 / multiplier / multiplier)
-            print gon, gonNew, gon.color, color
-            pImg.drawPolygon(gonNew, color = color, offset = multiplier)
+        color = colorFromPath3(gon.gon.center.z, gon.path, 1.0 / multiplier / multiplier)
+        print gon, gonNew, gon.color, color
+        pImg.drawPolygon(gonNew, color = color, offset = multiplier)
     return pImg
+
 
 
 p = poincare.p
@@ -178,25 +155,22 @@ t1 = 1 - np.tan(np.pi / p) * np.tan(np.pi / q)
 t2 = 1 + np.tan(np.pi / p) * np.tan(np.pi / q)
 
 gonRadius = np.sqrt(t1 / t2)
-
 gonFundamental = poincare.polygon(poincare.origin, verticeCount = 5, radius = gonRadius)
-
 gons = {}
 gons[""] = gonClass(p, q, gonRadius, [])
-gonsTable = [gonClass(p, q, gonRadius, [])]
-
 
 depth = poincare.depth
 for i in range(depth):
     iterLabels(i)
 
-#print gons.keys()
-gonsTable.sort(key = lambda x: x.label)
+gonsTable = [gons[l] for l in gons]
+gonsTable.sort(key = lambda x: x.direction)
 print [g.label for g in gonsTable]
 
 ims = []
 
 frameCount = poincare.frameCount
+zoomFactor = poincare.zoom
 
 mpInit = 10.0 ** (1.0 / frameCount)
 ts = np.linspace(0.0, 1.0, frameCount)
@@ -218,10 +192,12 @@ frameN = 0
 for mp in mps:
     frameN += 1
     print "frame {}".format(frameN)
-    pImg = iterImg(mp)
+    pImg = iterImg(mp, reverse = False, radiusFactor = poincare.zoom)
     if poincare.testMode:
+#        showImg(pImg, f = zoomFactor)
         showImg(pImg)
     if poincare.outputFile:
+#        ims.append(getImg(pImg, color = "bgr"), f = zoomFactor)
         ims.append(getImg(pImg, color = "bgr"))
 
 
